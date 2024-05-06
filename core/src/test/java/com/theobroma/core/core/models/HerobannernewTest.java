6dde6c17fe735e9ceb515e8f4999d61118c8af72
jeegitnew <jee290cts@gmail.com>
2024-05-06 13:36:41 +0530

    second commit

---
+++ core/src/test/java/com/theobroma/core/core/models/HerobannernewTest.java
@@ -0,0 +1,14 @@
+package com.theobroma.core.core.models;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.theobroma.core.core.testcontext.AppAemContext;
+
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+
+@ExtendWith(AemContextExtension.class)
+public class HerobannernewTest {
+    private final AemContext context = AppAemContext.newAemContext();
+    
+}
